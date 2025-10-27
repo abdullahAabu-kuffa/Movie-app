5c1e1eb5b1133fdd7bfca6525cb007c9a92eb603
moemad <moemad504@gmail.com>
2025-10-27 15:20:19 +0300

    create Movie Details Screen

--- lib/core/routing/movie_router.dart
+++ lib/core/routing/movie_router.dart
@@ -1,15 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:movie_app/ui/screens/home_screen.dart';
+import 'package:movie_app/ui/screens/movie_details.dart';
 import 'package:movie_app/ui/screens/onboarding_screen.dart';
 
 //app router 
 class MovieRouter {
   static Route<dynamic> generateRoute(RouteSettings settings) {
+
+    
+    // final MoviePopular mockMovie = MoviePopular(
+    //   name: "Test Movie Title",
+    //   description: "This is a temporary description for debugging the Details Screen.",
+    //   image: "assets/images/onboarding.png", 
+    // );
+
+    
     switch (settings.name) {
+
       case Routes.initialRoute:
         return MaterialPageRoute(builder: (_) => const OnboardingScreen());
       case Routes.homeRoute:
         return MaterialPageRoute(builder: (_) => const HomeScreen());
+      // case Routes.initialRoute:
+        // return MaterialPageRoute(builder: (_) => MovieDetails(movie:   mockMovie));
       default:
         return MaterialPageRoute(builder: (_) => const OnboardingScreen());
     }
